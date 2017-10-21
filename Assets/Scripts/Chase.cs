e2d2d28be8ea8e4cd077a87dee7d03c7f02be320
Ryan Lindsay <lindsa48@students.rowan.edu>
2017-10-20 20:43:53 -0400

    Added Chase script
    Added a script for enemys that will chase down the player when the player gets to close.

---
+++ Assets/Scripts/Chase.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Chase : MonoBehaviour {
+	private GameObject player;
+	private float distance;
+	//public Transform initialPosition;
+	public float moveSpeed;
+	public float visionRange;
+
+	// Use this for initialization
+	void Start () {
+		player = GameObject.FindGameObjectWithTag ("Player");
+		//transform.position = initialPosition.position;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		distance = Vector3.Distance (transform.position, player.transform.position);
+
+		if (distance < visionRange) {
+			transform.position = Vector3.MoveTowards (transform.position, player.transform.position, moveSpeed * Time.deltaTime);
+		}
+	}
+}
